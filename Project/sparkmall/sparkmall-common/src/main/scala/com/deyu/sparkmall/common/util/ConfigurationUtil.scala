352be5b463c2593fb1d0ef4cd7670ce3cb748682
zhangdeyu <1432360132@qq.com>
2019-06-18 16:50:03 +0800

    2019-06-18 1.add sparkmall project 2.环境准备

---
+++ Project/sparkmall/sparkmall-common/src/main/scala/com/deyu/sparkmall/common/util/ConfigurationUtil.scala
@@ -0,0 +1,25 @@
+package com.deyu.sparkmall.common.util
+
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder
+import org.apache.commons.configuration2.builder.fluent.Parameters
+import org.apache.commons.configuration2.{FileBasedConfiguration, PropertiesConfiguration}
+
+object ConfigurationUtil {
+
+  def apply(propertiesName: String) = {
+    val configurationUtil = new ConfigurationUtil()
+    if (configurationUtil.config == null) {
+      configurationUtil.config = new FileBasedConfigurationBuilder[FileBasedConfiguration](classOf[PropertiesConfiguration])
+        .configure(new Parameters().properties().setFileName(propertiesName)).getConfiguration
+    }
+    configurationUtil
+  }
+
+}
+
+class ConfigurationUtil() {
+  var config: FileBasedConfiguration = null
+
+}
+
+
---
+++ Project/sparkmall/src/main/scala/com/deyu/test/test1.scala
@@ -0,0 +1,8 @@
+package com.deyu.test
+
+object test1 {
+  def main(args: Array[String]): Unit = {
+    println("aa")
+  }
+
+}
